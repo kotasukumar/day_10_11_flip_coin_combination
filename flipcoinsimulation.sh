2875e2feaef5b88c8fb6c624c26e3f062ffb8f68
sukumarkindia424@gmail.com <sukumarkindia424@gmail.com>
2022-02-24 17:34:41 +0530

    added:dobule origin logic

--- flipcoinsimulation.sh
+++ flipcoinsimulation.sh
@@ -16,4 +16,39 @@ do
 done
 echo ${singlet[@]}
 head_percent=$(($head_count/$i*100))
-echo "head count and percentage :" $head_count $head_percent|bc
+tail_percent=$(($tail_count/$i*100))
+echo "head count and percentage :" $head_count $head_percent
+echo "tail count and percentage:" $tail_count $tail_percent
+#doublet logic
+hh=0
+ht=0
+th=0
+tt=0
+        for((i=0;i<20;i++))
+        do
+        coin1=$((RANDOM%2))
+        coin2=$((RANDOM%2))
+                if(($coin1==0 && $coin2==0))
+                then
+                        doublet[$i]="(head-head)   "
+			((hh++))
+                elif(($coin1==0 && $coin2==1))
+                then
+			doublet[$i]="(head-tail)   "
+                        ((ht++))
+                elif(($coin1==1 && $coin2==0))
+                then
+			doublet[$i]="(tail-head)   "
+                        ((th++))
+                elif(($coin1==1 && $coin2==1))
+                then
+			doublet[$i]="(tail-tail)   "
+                        ((tt++))
+                fi
+        done
+
+echo ${doublet[@]}
+echo "head-head count and percentage is:" $hh $(($hh/$i*100))
+echo "head-tail count and percentage is:" $ht $(($ht/$i*100))
+echo "tail-head count and percentage is:" $th $(($th/$i*100))
+echo "tail-tail count and percentage is:" $tt $(($tt/$i*100))
